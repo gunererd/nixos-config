67d2e6c42c6462e0a218e15d765d27e4b768c105
erdem <gunererd@gmail.com>
2025-06-05 21:03:25 +0300

    add global git username and email

--- programs/git.nix
+++ programs/git.nix
@@ -5,6 +5,10 @@
   programs.git = {
     enable = true;
     package = pkgs.git;
+    
+    # Global git configuration
+    userName = "erdem";
+    userEmail = "gunererd@gmail.com";
   };
   
   # Add git to system packages
