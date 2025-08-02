083c23a19c9d229ed8e4e56ed3b05da8f7286620
erdem <gunererd@gmail.com>
2025-08-02 22:16:54 +0300

    fix fish.nix

--- programs/fish.nix
+++ programs/fish.nix
@@ -1,12 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  programs.fish.enable = true;
-  environment.shells = [ pkgs.fish ];
-  users.defaultUserShell = pkgs.fish;
-
-  programs.starship = {
+  programs.fish = {
     enable = true;
-    enableFishIntegration = true;
+    interactiveShellInit = ''
+      starship init fish | source
+    '';
   };
+
+  programs.starship.enable = true;
+
+  # Optional: set fish as the default shell
+  users.defaultUserShell = pkgs.fish;
+  environment.shells = [ pkgs.fish ];
 }
