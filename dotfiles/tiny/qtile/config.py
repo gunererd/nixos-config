ef918efa2e20307008aaf4ea4f7cc4e35c5e3997
erdem <gunererd@gmail.com>
2025-09-28 16:07:19 +0300

    fix click to bring window front for tiny

--- dotfiles/tiny/qtile/config.py
+++ dotfiles/tiny/qtile/config.py
@@ -159,12 +159,13 @@ mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
+    Click([], "Button1", lazy.window.bring_to_front()),
 ]
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: list
 follow_mouse_focus = True
-bring_front_click = False
+bring_front_click = True
 floats_kept_above = True
 cursor_warp = False
 floating_layout = layout.Floating(
